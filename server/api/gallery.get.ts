9ea58933978d58b7fb6ae790628d9203d3253c43
suku-kahanamoku <sukusovi@gmail.com>
2024-05-13 11:48:32 +0200

    Implement gallery.

--- server/api/gallery.get.ts
+++ server/api/gallery.get.ts
@@ -2,13 +2,12 @@ import fs from 'fs';
 import sizeOf from 'image-size';
 
 export default defineEventHandler(async (event) => {
-	const images = await fs.readdirSync(`${__dirname}`);
-	return images;
+	const images = await fs.readdirSync('./client/_nuxt');
 	return images
 		?.filter((path) => path.includes('.jpg'))
 		?.map((path) => {
 			const result: any = { src: `/gallery/${path}` };
-			const { height, width } = sizeOf(`${process.cwd()}/_nuxt/${path}`);
+			const { height, width } = sizeOf(`./client/_nuxt/${path}`);
 			result.height = height;
 			result.width = width;
 			return result;
