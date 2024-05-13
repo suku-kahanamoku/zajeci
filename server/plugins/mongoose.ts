4b153a93be05ae1c96c90f67eefc87e808ad2b48
suku-kahanamoku <sukusovi@gmail.com>
2024-05-13 17:04:03 +0200

    Implement db connection.

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export default defineNitroPlugin(async (nitroApp) => {
 	try {
 		await mongoose.connect(
-			`mongodb+srv://${process.env.MONGO_ADM_USER}:${process.env.MONGO_ADM_PASS}@${process.env.MONGO_HOST}`,
+			`mongodb+srv://${process.env.MONGO_ADM_USER}:${process.env.MONGO_ADM_PASS}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority&appName=Test`,
 			{ dbName: process.env.MONGO_DB }
 		);
 		console.log('Connected to MongoDB.');
