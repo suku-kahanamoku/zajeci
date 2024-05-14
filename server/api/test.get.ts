ec48355fe33b571a753406d06f5b6aee138c6960
suku-kahanamoku <sukusovi@gmail.com>
2024-05-14 14:51:05 +0200

    Dummy.

--- server/api/test.get.ts
+++ server/api/test.get.ts
@@ -1,5 +1,5 @@
 import { H3Event } from 'h3';
 
 export default defineEventHandler(async (event: H3Event) => {
-	return event.node.req.socket.remoteAddress || event.node.req.headers['x-forwarded-for'];
+	return event.context.ip || event.node.res.socket?.remoteAddress || event.node.res.socket?.remoteAddress;
 });
