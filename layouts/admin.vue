1670ccdbc1e8669ab22b753836d76358996b46bd
suku-kahanamoku <sukusovi@gmail.com>
2024-05-14 18:26:21 +0200

    Implement product admin.

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,6 +1,19 @@
+<script setup lang="ts">
+	const menuItems = [
+		{
+			label: '$.dashboard.title',
+			to: '/admin',
+		},
+		{
+			label: '$.admin.wine.title',
+			to: '/admin/wine',
+		},
+	];
+</script>
+
 <template>
 	<div class="min-h-screen flex flex-col">
-		<UiNavbar />
+		<UiNavbar :menuItems="menuItems" />
 
 		<main class="flex-1 flex">
 			<slot></slot>
---
+++ pages/admin/wine/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+	definePageMeta({
+		layout: 'admin',
+		syscode: 'admin_wine',
+		title: '$.dashboard.title',
+	});
+
+	const { t, locale } = useI18n();
+
+	useHead({
+		title: `${t('$.base.title')} | ${t('$.dashboard.title')}`,
+		meta: [
+			{ name: 'description', content: t('$.base.description') },
+			{ name: 'keywords', content: t('$.base.description') },
+		],
+	});
+</script>
+
+<template>
+	<div class="max-w-screen-xl mx-auto px-5 w-full">
+		<div id="dashboard" class="py-10">
+			<h1
+				class="text-center text-primary-600 text-4xl lg:text-5xl font-bold tracking-tight dark:text-primary-400 pb-8"
+			>
+				{{ $t('$.admin.wine.title') }}
+			</h1>
+		</div>
+	</div>
+</template>
