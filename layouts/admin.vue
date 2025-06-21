9c3b14f6ad4106511780a5189a2478df51523f27
Suche Kahanamoku <sukusovi@gmail.com>
2025-06-21 16:28:34 +0200

    Implement order.

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -3,15 +3,18 @@ const { menuItem, routes } = useMenuItems();
 const localePath = useLocalePath();
 const auth = useAuthStore();
 
-const admin = menuItem("admin")!;
-admin.to = localePath(admin.to!);
-const menuItems = [admin];
+const menuItems: any[] = [];
 
 if (auth.isAdmin) {
   const adminWine = menuItem("admin_wine")!;
   adminWine.to = localePath(adminWine.to!);
   menuItems.push(adminWine);
+
+  const adminOrder = menuItem("admin_order")!;
+  adminOrder.to = localePath(adminOrder.to!);
+  menuItems.push(adminOrder);
 }
+console.log(menuItems);
 </script>
 
 <template>
@@ -19,7 +22,18 @@ if (auth.isAdmin) {
     <UiNavbar :menuItems="menuItems" />
 
     <main class="flex-1 flex">
-      <slot></slot>
+      <UNavigationMenu
+        orientation="vertical"
+        :items="menuItems"
+        :highlight="true"
+        :ui="{
+          root: 'border-r border-gray-200',
+        }"
+      />
+
+      <div class="w-full">
+        <slot></slot>
+      </div>
     </main>
 
     <CmpFooter url="https://www.prasentace.cz/" name="PRASENTACE" />
