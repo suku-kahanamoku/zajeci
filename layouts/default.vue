4d0e5f81dd2550298fe0bd86dfcf2259d069c106
Suche Kahanamoku <sukusovi@gmail.com>
2025-10-21 07:57:53 +0200

    Upgrade.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+const { routes } = useMenuItems();
 const localePath = useLocalePath();
+const userSessionStore = useUserSession();
 
 useSeoMeta({
   robots: "index, follow",
@@ -47,6 +49,37 @@ const menuItems = [
 
     <template #right>
       <UColorModeButton />
+
+      <div class="space-x-2">
+        <UButton
+          v-if="routes?.login?.path"
+          data-testid="menu-login"
+          :to="localePath(routes?.login?.path)"
+          class="text-secondary-500 dark:text-secondary-400"
+          variant="outline"
+          active-class="hidden"
+        >
+          {{
+            $tt(
+              (routes?.login?.meta?.label ||
+                routes?.login?.meta?.title) as string
+            )
+          }}
+        </UButton>
+        <UButton
+          v-if="routes?.signup?.path"
+          data-testid="menu-signup"
+          :to="localePath(routes?.signup?.path)"
+          active-class="hidden"
+        >
+          {{
+            $tt(
+              (routes?.signup?.meta?.label ||
+                routes?.signup?.meta?.title) as string
+            )
+          }}
+        </UButton>
+      </div>
     </template>
 
     <template #body>
--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@ useHead({
     <div class="max-w-screen-xl mx-auto px-5 w-full">
       <CustomAbout />
       <USeparator />
-      <!-- <CmpWineListTop /> -->
+      <CmpWineListTop />
       <USeparator />
       <CustomTasting />
       <USeparator />
