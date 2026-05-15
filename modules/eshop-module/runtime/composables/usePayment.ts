9784acf0b1cacf9ee82e48e394e060ffb8ba8b1f
Suku <sukusovi@gmail.com>
2026-05-15 22:15:27 +0200

    Update.

--- modules/eshop-module/runtime/composables/usePayment.ts
+++ modules/eshop-module/runtime/composables/usePayment.ts
@@ -23,27 +23,25 @@ function createPayment() {
     (enumPayments.value ?? [])
       .filter((e) => e.published)
       .map((e) => ({
-        label:    e.label,
-        price:    totalPrice.value > 2500 ? 0 : (e.data?.price ?? 0),
-        icon:     e.data?.icon,
+        label: e.label,
+        price: totalPrice.value > 2500 ? 0 : (e.data?.price ?? 0),
+        icon: e.data?.icon,
         disabled: e.data?.disabled ?? false,
-        value:    e.value ?? e.syscode,
+        value: e.value ?? e.syscode,
       })),
   );
 
   function setPayment(newPayment?: IPayment | null) {
     const fallback =
       paymentOptions.value.find((p) => !p.disabled) ?? paymentOptions.value[0];
-    payment.value = CLONE(
-      newPayment || fallback || ({} as IPayment),
-    );
+    payment.value = CLONE(newPayment || fallback || ({} as IPayment));
   }
 
   // Nastav výchozí platbu jakmile jsou načteny options
   watch(
     paymentOptions,
     (opts) => {
-      if (opts.length && !payment.value.type) {
+      if (opts.length && !payment.value.value) {
         setPayment();
       }
     },
